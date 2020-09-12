45cee58e0987f5e2fc5f03722887ef2ff5aca6cd
刘梦龙 <l893043230@163.com>
2020-09-12 11:11:39 +0800

    初始化项目

--- src/components/personalcenter/item/ItemFather.vue
+++ src/components/personalcenter/item/ItemFather.vue
@@ -11,7 +11,18 @@
 </script>
 
 <style scoped>
-.father{
+    .father {
 
-}
+        display: flex;
+        background-color: #f6f6f6;
+
+        /*底部*/
+        position: fixed;
+        left: 0;
+        right: 0;
+        bottom: 0;
+
+        box-shadow: 0px -2px 1px rgba(100, 100, 100, .1);
+
+    }
 </style>
